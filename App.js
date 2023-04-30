13916df13f21ca1a2aeb87b2f9122b33efc3b1ae
Sergey Chistiakov <chistykov2024@gmail.com>
2023-04-30 18:18:13 +0300

    дз1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello! I am Sergey Chistyakov</Text>
       <StatusBar style="auto" />
     </View>
   );
